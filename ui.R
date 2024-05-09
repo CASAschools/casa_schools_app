5ef578ff8a416ebf892c5d7e0f7987b95ccd9209
hazelvaq <hvaquero98@gmail.com>
2024-05-09 20:41:23 +0000

    edit ui skeleton placement

--- ui.R
+++ ui.R
@@ -12,13 +12,13 @@ sidebar <- dashboardSidebar(width = 300,
     menuItem(text = h4("Welcome"), tabName = "welcome", icon = icon("star")),
     menuItem(text = h4("Hazard Summary Metric"), tabName = "index", icon = icon("square-poll-horizontal")),
     menuItem(text = h4("Explore Your Hazards"), tabName = "hazards", icon = icon("earth-americas"),
+      # Explore Your Hazards Sub tabs
       menuSubItem(text = h4("Extreme Heat"), tabName = "heat", icon = icon("temperature-arrow-up")),
       menuSubItem(text = h4("Wildfire"), tabName = "wildfire", icon = icon("fire")),
       menuSubItem(text = h4("Extreme Precipitation"), tabName = "precipitation", icon = icon("cloud-showers-heavy")),
       menuSubItem(text = h4("Flooding"), tabName = "flooding", icon = icon("house-flood-water")),
       menuSubItem(text = h4("Sea Level Rise"), tabName = "sea_rise", icon = icon("house-tsunami"))
     ),
-    menuItem(text = h4("User Documentation"), tabName = "user", icon = icon("person")),
     menuItem(text = h4("Climate Information"), tabName = "info", icon = icon("circle-info")),
     menuItem(text = h4("Glossary"), tabName = "glossary", icon = icon("book-open")),
     menuItem(text = h4("User Guide"), tabName = "user", icon = icon("users"))
@@ -54,87 +54,11 @@ body <- dashboardBody(
     ),
     
 
-    
-    # ---------- Hazards tab -----------------------------
-    tabItem(tabName = "hazards",
-            fluidPage(
-              box(
-                width = NULL,
-                title = h2(tags$strong("Dos Pueblos Senior High")),
-                tabsetPanel(
-                  tabPanel(h4("Extreme Heat"),
-                           # Load extreme heat script
-                           source("hazards_tab/extreme_heat.R",
-                                  # Remove TRUE output
-                                  local = TRUE, 
-                                  echo = FALSE, 
-                                  print.eval = FALSE)[1]
-                           
-                           
-                           ),#END Extreme Heat
-
-                  tabPanel(h4("Extreme Precipitation"),
-                           # Load extreme precipitation script
-                           source("hazards_tab/extreme_precipitation.R", 
-                                  local = TRUE,
-                                  echo = FALSE,
-                                  print.eval = FALSE)[1]
-                           
-                           
-                           ),#END EXTREME PRECIPITATION
-                  
-                  tabPanel(h4("Wildfire"),
-                           # Load wildfire script
-                           source("hazards_tab/wildfire.R", 
-                                  local = TRUE,
-                                  echo = FALSE,
-                                  print.eval = FALSE)[1]
-                           
-                           
-                           ),#END WILDFIRE
-                  
-                  tabPanel(h4("Flooding"),
-                           # Load flooding script
-                           source("hazards_tab/flooding.R", 
-                                  local = TRUE,
-                                  echo = FALSE,
-                                  print.eval = FALSE)[1]
-                           
-                           ),#END FLOODING
-                  
-                  tabPanel(h4("Coastal Inundation"),
-                           # Load coastal inundation script
-                           source("hazards_tab/coastal_inundation.R", 
-                                  local = TRUE,
-                                  echo = FALSE,
-                                  print.eval = FALSE)[1]
-                           
-                           ),#END COASTAL INUNDATION
-                  
-                  tabPanel(h4("Hazard Summary"),
-                           #load hazard summary script
-                           source("hazards_tab/hazard_summary.R",
-                                  local = TRUE,
-                                  echo = FALSE,
-                                  print.eval = FALSE)[1]
-                           
-                           )#END HAZARD SUMMARY
-                
-              )#END Tabset Panel
-              
-            )#END Box
-    
-    )# END Fluid Pages
-    
-    ),#END HAZARDS TAB
-    
-    
-    # --------- Index tab --------------------------------   
+    # --------- Hazard Summary Metric tab --------------------------------   
     tabItem(tabName = "index",
             # Content for the index tab
             fluidPage(
-              
-              
+
               box(width = NULL,
                   title = h2(tags$strong("Dos Pueblos Senior High Hazards Summary Metric"))
               )
@@ -144,19 +68,187 @@ body <- dashboardBody(
     ),#END INDEX
     
     
-    # ------- Socioeconomic tab ---------------------
-    tabItem(tabName = "adapt",
+    # ---------- Explore Your Hazards Sub Tabs -----------------------------
+    
+    # --------- Extreme Heat Tab -------------------------------------------   
+    
+    tabItem(tabName = "heat",
+            fluidPage(
+              box(
+                width = NULL,
+                "fire"
+              )#END BOX
+              
+            )#END FluidPage
+      
+            ),#END HEAT TAB
+    
+    # --------- Wildfire Tab -----------------------------------------------
+    tabItem(tabName = "wildfire",
+            fluidPage(
+              box(
+                width = NULL,
+                "wildfire"
+              )# END BOX
+              
+            )#END FluidPage
+            
+            ),#END Wildfire
+    
+  
+    # --------- Precipitation Tab ------------------------------------------
+    
+    tabItem(tabName = "precipitation",
+            fluidPage(
+              box(
+                width = NULL,
+                "precip"
+              )#END BOX
+              
+            )#END FluidPage
+            
+    ),#END Flooding
+    
+    # --------- Flooding Tab------------------------------------
+    
+    tabItem(tabName = "flooding",
+            fluidPage(
+              box(
+                width = NULL,
+                "FLOODING"
+              )#END BOX
+              
+            )#END FluidPage
+            
+            ),#END Flooding
+    
+    # --------- Sea Level Rise Tab ------------------------------------------
+    
+    tabItem(tabName = "sea_rise",
+            fluidPage(
+              box(
+                width = NULL,
+                "sea level"
+              )#END BOX
+              
+            )#END FluidPage
+            
+    ),#END Sea Level Rise
+    
+    
+    # tabItem(tabName = "hazards",
+    #         fluidPage(
+    #           box(
+    #             width = NULL,
+    #             title = h2(tags$strong("Dos Pueblos Senior High")),
+    #             tabsetPanel(
+    #               tabPanel(h4("Extreme Heat"),
+    #                        # Load extreme heat script
+    #                        source("hazards_tab/extreme_heat.R",
+    #                               # Remove TRUE output
+    #                               local = TRUE, 
+    #                               echo = FALSE, 
+    #                               print.eval = FALSE)[1]
+    #                        
+    #                        
+    #                        ),#END Extreme Heat
+    # 
+    #               tabPanel(h4("Extreme Precipitation"),
+    #                        # Load extreme precipitation script
+    #                        source("hazards_tab/extreme_precipitation.R", 
+    #                               local = TRUE,
+    #                               echo = FALSE,
+    #                               print.eval = FALSE)[1]
+    #                        
+    #                        
+    #                        ),#END EXTREME PRECIPITATION
+    #               
+    #               tabPanel(h4("Wildfire"),
+    #                        # Load wildfire script
+    #                        source("hazards_tab/wildfire.R", 
+    #                               local = TRUE,
+    #                               echo = FALSE,
+    #                               print.eval = FALSE)[1]
+    #                        
+    #                        
+    #                        ),#END WILDFIRE
+    #               
+    #               tabPanel(h4("Flooding"),
+    #                        # Load flooding script
+    #                        source("hazards_tab/flooding.R", 
+    #                               local = TRUE,
+    #                               echo = FALSE,
+    #                               print.eval = FALSE)[1]
+    #                        
+    #                        ),#END FLOODING
+    #               
+    #               tabPanel(h4("Coastal Inundation"),
+    #                        # Load coastal inundation script
+    #                        source("hazards_tab/coastal_inundation.R", 
+    #                               local = TRUE,
+    #                               echo = FALSE,
+    #                               print.eval = FALSE)[1]
+    #                        
+    #                        ),#END COASTAL INUNDATION
+    #               
+    #               tabPanel(h4("Hazard Summary"),
+    #                        #load hazard summary script
+    #                        source("hazards_tab/hazard_summary.R",
+    #                               local = TRUE,
+    #                               echo = FALSE,
+    #                               print.eval = FALSE)[1]
+    #                        
+    #                        )#END HAZARD SUMMARY
+                
+    #           )#END Tabset Panel
+    #           
+    #         )#END Box
+    # 
+    # )# END Fluid Pages
+    # 
+    # ),#END HAZARDS TAB
+    
+    
+    # ------- Climate Information tab -----------------------------------
+    # this will describe time concepts, how, why, etc...
+    
+    tabItem(tabName = "info",
             fluidPage(
               box(width = NULL,
                   includeMarkdown("text/socio.md"),
               )
             )#END FLUIDPAGE
-    ),#END SOCIOECONOMIC TAB
+    ),#END Climate Info TAB
+    
+    
+    # ------- Glossary tab ---------------------------------------------
+    tabItem(tabName = "glossary",
+            fluidPage(
+              box(width = NULL
+                  
+              )# END Box
+              
+            )# END FluidPage
+            
+            
+    ),#END Glossary
+    
+    
+    
+
     # ------- User guide tab -----------------------
-    tabItem(tabName = "guide",
+    tabItem(tabName = "user",
+            fluidPage(
+              box(width = NULL
+                  
+                  )# END Box
+              
+            )# END FluidPage
+            
+            
     )#END USER GUIDE
     
-  ),#END TABS CONTENT
+  )#END TABS CONTENT
   
 )#END BODY
 
